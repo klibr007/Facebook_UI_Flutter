8a38394626ef363224105ec959997214ffc94ada
klibr007 <davidlafia@yahoo.fr>
2021-08-01 16:10:58 +0200

    end of stories widget

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -47,7 +47,15 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
-
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 05.0),
+            sliver: SliverToBoxAdapter(
+              child: Stories(
+                currentUser: currentUser,
+                stories: stories,
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/screens/home_screen/widgets/profile_avatar.dart
+++ lib/screens/home_screen/widgets/profile_avatar.dart
@@ -5,8 +5,13 @@ import 'package:flutter/material.dart';
 class ProfileAvatar extends StatelessWidget {
   final String imageUrl;
   final bool isActive;
+  final bool hasBorder;
 
-  const ProfileAvatar({Key? key, required this.imageUrl, this.isActive = false})
+  const ProfileAvatar(
+      {Key? key,
+      required this.imageUrl,
+      this.isActive = false,
+      this.hasBorder = false})
       : super(key: key);
 
   @override
@@ -15,22 +20,27 @@ class ProfileAvatar extends StatelessWidget {
       children: [
         CircleAvatar(
           radius: 20.0,
-          backgroundColor: Colors.grey[200],
-          backgroundImage: CachedNetworkImageProvider(imageUrl),
-        ),
-        isActive ? Positioned(
-          bottom: 0.0,
-          right: 0.0,
-          child: Container(
-            height: 15.0,
-            width: 15.0,
-            decoration: BoxDecoration(
-              color: Palette.online,
-              shape: BoxShape.circle,
-              border: Border.all(width: 2.0, color: Colors.white)
-            ),
+          backgroundColor: Palette.facebookBlue,
+          child: CircleAvatar(
+            radius: hasBorder ? 17.0 : 20.0,
+            backgroundColor: Colors.grey[200],
+            backgroundImage: CachedNetworkImageProvider(imageUrl),
           ),
-        ) : const SizedBox.shrink(),
+        ),
+        isActive
+            ? Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  height: 15.0,
+                  width: 15.0,
+                  decoration: BoxDecoration(
+                      color: Palette.online,
+                      shape: BoxShape.circle,
+                      border: Border.all(width: 2.0, color: Colors.white)),
+                ),
+              )
+            : const SizedBox.shrink(),
       ],
     );
   }
